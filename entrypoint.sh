7624ad61002b035ae6e26143bfce422e7598cead
Niraj Shah <niraj3k@gmail.com>
2022-05-20 19:51:45 +0100

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 search_result=$(egrep -r -i 'print(.*)\(user!?.token' $GITHUB_WORKSPACE)
 
+echo $search_result
+
 if [[ $search_result ]]; then
     echo "Potential tokens found! \n $search_result"
     exit 1
