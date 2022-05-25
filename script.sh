b18718457105af52e2d8ea7bb91b44b9fe985aa4
Niraj Shah <niraj3k@gmail.com>
2022-05-25 16:35:02 +0100

    Updated script and action

--- script.sh
+++ script.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-if egrep -r -i 'print(.*)\(user!?.token' $GITHUB_WORKSPACE; then
-    echo "Potential tokens found! \n $search_result"
+# find uncommented print statements
+tokens=$(egrep -r -i '^([^//]*)\s?print([a-zA-Z]*)\(\$?(user)?(!)?(.)?token' $GITHUB_WORKSPACE)
+
+if [ -n "$tokens" ]; then
+    while IFS= read -r line
+    do
+        echo "::warning::$line"
+    done <<< "$tokens"
+
+    echo "::error::Potential tokens found!"
     exit 1
 else
-    echo "Eveything *looks* ok!"
+    echo "::notice::Eveything *looks* ok!"
     exit 0
 fi
